a3d944c7f3dec177f6cfd3c694e09b0d4662dddd
Seshadrireddy <115624876+sesadrireddybhavanasi@users.noreply.github.com>
2022-12-13 17:22:04 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum upgrade -y
+amazon-linux-extras install java-openjdk11 -y
+yum install jenkins -y
+systemctl start jenkins
+systemctl enable jenkins
